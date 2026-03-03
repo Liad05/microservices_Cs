cdf4e2b4867a450820cedb04c7d0e7b30c2165a7
Liad05 <liadgam10@gmail.com>
2026-03-03 13:58:23 +0200

    initial commit with code
    midway into module 2, added .gitignore

---
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDTO> items = new()
+        {
+            new ItemDTO(Guid.NewGuid(),"Potion","Restores a small amount of HP",5,DateTimeOffset.UtcNow),
+            new ItemDTO(Guid.NewGuid(),"Antidote","Cures poison affect",7,DateTimeOffset.UtcNow),
+            new ItemDTO(Guid.NewGuid(),"Bronze sword","Deals a amall amount of damage",20,DateTimeOffset.UtcNow),
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDTO> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ItemDTO GetById(Guid id)
+        {
+            var item = items.Where(item => item.id == id).SingleOrDefault();
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDTO> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDTO(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+            return CreatedAtAction(nameof(GetById), new { id = item.id }, item);
+        }
+    }
+}
---
+++ Play.Catalog/src/Play.Catalog.Service/DTOs.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Play.Catalog.Service.DTOs
+{
+    public record ItemDTO(Guid id, string Name, string Description, decimal Price, DateTimeOffset CreateDate);
+    public record CreateItemDto(string Name, string Description, decimal Price);
+    public record UpdateItemDto(string Name, string Description, decimal Price);
+}
